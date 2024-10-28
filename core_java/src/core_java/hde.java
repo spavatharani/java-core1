c4bb9f3da4cfe1bd176cd678e8570f6a8244ecbd
spavatharani <ELCOT@Lenovo>
2024-10-28 14:49:28 +0530

    First Commit

---
+++ core_java/src/core_java/hde.java
@@ -0,0 +1,10 @@
+package core_java;
+
+public class hde {
+
+	public static void main(String[] args) {
+		System.out.println("java programming class");
+
+	}
+
+}
---
+++ core_java/src/core_java/java.java
@@ -0,0 +1,10 @@
+package core_java;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
